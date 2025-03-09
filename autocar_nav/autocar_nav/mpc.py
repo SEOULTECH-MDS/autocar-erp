4d98980152bee3c51837993f75fe5ef4ea2046da
YSL2683 <dldbstjd5566@gamil.com>
2025-03-09 16:19:51 +0900

    0309실험 중 변경 내용

--- autocar_nav/autocar_nav/mpc.py
+++ autocar_nav/autocar_nav/mpc.py
@@ -46,8 +46,8 @@ WB = 1.566  # [m] default : 2.5, ERP42 : 1.566
 
 MAX_STEER = np.deg2rad(30.0)  # maximum steering angle [rad]  default: 45
 MAX_DSTEER = np.deg2rad(30.0)  # maximum steering speed [rad/s]
-MAX_SPEED = 1.0  # maximum speed [m/s]
-MIN_SPEED = -1.0  # minimum speed [m/s]
+MAX_SPEED = 1.5  # maximum speed [m/s]
+MIN_SPEED = -1.5  # minimum speed [m/s]
 MAX_ACCEL = 0.614  # maximum accel [m/ss] defualt: 1.0
 
 # show_animation = True
