ee3e7edd1a24f8213bd3315d4eca2e2a363c66ca
YSL2683 <dldbstjd5566@gmail.com>
2025-07-14 20:43:00 +0900

    control import 수정

--- src/core/control/main_control/setup.py
+++ src/core/control/main_control/setup.py
@@ -11,7 +11,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=['setuptools', 'autocar_utils'],
     zip_safe=True,
     maintainer='ysl',
     maintainer_email='dldbstjd5566@gmail.com',
