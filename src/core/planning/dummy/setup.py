39405dbea70775e249501ede6d3d793d609430dd
baebling <gangmin4234@gmail.com>
2025-09-10 22:41:10 +0900

    주차 launch.py

--- src/core/planning/dummy/setup.py
+++ src/core/planning/dummy/setup.py
@@ -5,10 +5,20 @@ package_name = 'dummy'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[],
+    # ament_python이 lib/<pkg> 구조와 런치에서 찾는 libexec 경로를 생성할 수 있도록 패키지를 설치
+    packages=[package_name],
+    package_dir={
+        package_name: '.',
+    },
+    # 모듈은 패키지(dummy) 네임스페이스 하위로 설치됩니다
     data_files=[
+        # ament index 패키지 마커
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/test_parking_area.launch.py']),
+        ('share/' + package_name + '/launch', [
+            'launch/test_parking_area.launch.py',
+            'launch/cones_pipeline.launch.py',
+        ]),
         ('share/' + package_name + '/rviz', ['rviz/parking_area.rviz']),
     ],
     install_requires=['setuptools', 'numpy>=1.21.0', 'scikit-learn>=1.0.0'],
@@ -20,11 +30,13 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'cones_node = cones_node:main',
-            'parking_area_node = parking_area_node:main',
-            'initial_pose_node = initial_pose_node:main',
-            'odometry_node = odometry_node:main',
-            'road_node = road_node:main',
+            'cones_node = dummy.cones_node:main',
+            'parking_area_node = dummy.parking_area_node:main',
+            'initial_pose_node = dummy.initial_pose_node:main',
+            'odometry_node = dummy.odometry_node:main',
+            'road_node = dummy.road_node:main',
+            'cone_map_node = dummy.cone_map_node:main',
+            'rubber_cones_adapter_node = dummy.rubber_cones_adapter_node:main',
         ],
     },
 )
