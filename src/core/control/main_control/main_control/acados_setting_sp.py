1b982919e133177d276201f095243b2e0fd01cf1
YSL2683 <dldbstjd5566@gmail.com>
2025-10-22 19:19:13 +0900

    mpc 장애물 회피 에러, r_safe 제약설정 변경

--- src/core/control/main_control/main_control/acados_setting_sp.py
+++ src/core/control/main_control/main_control/acados_setting_sp.py
@@ -31,7 +31,7 @@ def acados_solver():
     ND = 1 # 이전 조향각 입력 크기 (delta)
     NV = 2 # tangent vector size (tx, ty)
     NW = 7 # cost function weights 크기 (W_acc, W_steer, W_steer_rate, W_v, W_lag, W_con, W_yaw)
-    NO = 9  # 장애물 정보 크기 (obs1_x, obs1_y, obs2_x, obs2_y, obs3_x, obs3_y, obs4_x, obs4_y, r_safe)
+    NO = 8  # 장애물 정보 크기 (obs1_x, obs1_y, obs2_x, obs2_y, obs3_x, obs3_y, obs4_x, obs4_y)
 
     T = 2.0
     N = 20  # 예측 구간 [s]
@@ -99,7 +99,7 @@ def acados_solver():
     We_v = W_v   # terminal cost에서 속도 error 가중치
     We_lag = W_lag   # terminal cost에서 lag error 가중치
     We_con = W_con  # terminal cost에서 contour error 가중치
-    We_yaw = W_yaw * 1.5 # heading error 가중치 (terminal cost)
+    We_yaw = W_yaw # heading error 가중치 (terminal cost)
 
     obs1_x = ocp.model.p[15]  # 장애물1 x 좌표
     obs1_y = ocp.model.p[16]  # 장애물1 y 좌표
@@ -109,8 +109,7 @@ def acados_solver():
     obs3_y = ocp.model.p[20]  # 장애물3 y 좌표
     obs4_x = ocp.model.p[21]  # 장애물4 x 좌표
     obs4_y = ocp.model.p[22]  # 장애물4 y 좌표
-    r_safe = ocp.model.p[23]  # 장애물 회피 반경
-    # TODO: r_safe를 고정값 써야할듯
+
 
     vehicle_x = ocp.model.x[0]  # 차량 x 좌표
     vehicle_y = ocp.model.x[1]  # 차량 y 좌표
