33bcb6ee4094c044e1a8edff612f4198876844be
YSL2683 <dldbstjd5566@gamil.com>
2025-10-25 13:15:11 +0900

    main_control bulid error

--- src/core/control/main_control/setup.py
+++ src/core/control/main_control/setup.py
@@ -12,7 +12,7 @@ setup(
         ('share/' + package_name, ['package.xml']),
         
         ('share/' + package_name + '/launch', [
-            'launch/mpc_obstacle_map.launch.py'
+            'launch/mpc_mapping.launch.py'
         ]),
     ],
     install_requires=['setuptools', 'autocar_utils'],
@@ -25,7 +25,6 @@ setup(
         'console_scripts': [
             'mpc_acados_control = main_control.mpc_acados:main',
             'mpc_acados_control_sp = main_control.mpc_acados_sp:main',
-            'mpc_acados_control_sp_traffic = main_control.mpc_acados_sp_traffic:main',
             'obstacle_map_node = main_control.obstacle_map_node:main',
             'deliverysign_map_node = main_control.deliverysign_map_node:main'
 
